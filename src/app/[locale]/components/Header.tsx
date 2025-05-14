97faa086352e333140964bf60aa1e8d58229a923
Andrés Meoñez <76024612+JoseanMeonez@users.noreply.github.com>
2025-05-14 00:11:30 -0600

    feat: update default logo

--- src/app/[locale]/components/Header.tsx
+++ src/app/[locale]/components/Header.tsx
@@ -3,8 +3,8 @@ import { Link } from '@/src/navigation'
 import { useTranslations } from 'next-intl'
 import { FC } from 'react'
 import GithubIcon from '../../icons/github'
-import LogoIcon from '../../icons/logo'
 import LangSwitcher from './LangSwitcher'
+import { PiCode } from 'react-icons/pi';
 interface Props {
   locale: string
 }
@@ -14,23 +14,22 @@ export const Header: FC<Props> = ({ locale }) => {
     <div className='mx-auto flex max-w-screen-2xl flex-row items-center justify-between p-5'>
       <Link lang={locale} href='/'>
         <div className='flex flex-row items-center'>
-          <div className='mb-2 h-14 w-14'>
-            <LogoIcon />
+          <div className='mr-2 h-8 w-8'>
+            <PiCode className='h-full w-full' />
           </div>
-          <strong className='mx-2 select-none'>Andrés Meoñez</strong>
+          <strong className='select-none text-xl'>Andrés Meoñez</strong>
         </div>
       </Link>
       <div className='flex flex-row items-center gap-3'>
         <nav className='mr-10 inline-flex gap-5'>
-          <Link lang={locale} href='/about'>{t('About')}</Link>
+          <Link lang={locale} href='/about'>
+            {t('About')}
+          </Link>
           {/* <Link lang={locale} href='#projects'>{t('Projects')}</Link>
           <Link lang={locale} href='#contact'>{t('Contact')}</Link> */}
         </nav>
         <LangSwitcher />
-        <a
-          href='https://github.com/JoseanMeonez'
-          target='_blank'
-        >
+        <a href='https://github.com/JoseanMeonez' target='_blank'>
           <div className='size-8'>
             <GithubIcon />
           </div>
